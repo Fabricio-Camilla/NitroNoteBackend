1343fb077f9dd7bcb17f840e9f48ff7e80ac6452
Fabricio Camilla <142159089+Fabricio-Camilla@users.noreply.github.com>
2025-09-01 14:41:40 -0300

    initial commit

---
+++ src/test/java/com/nitro_note/NitroNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitro_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitroNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

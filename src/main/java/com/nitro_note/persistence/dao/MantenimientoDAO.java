acab49d650239f7b0edd7083e8dd52ed198c9609
Leandro <leandropeppe@gmail.com>
2025-09-04 14:30:42 -0300

    Ahora el DAO depende del jpadto

--- src/main/java/com/nitro_note/persistence/dao/MantenimientoDAO.java
+++ src/main/java/com/nitro_note/persistence/dao/MantenimientoDAO.java
@@ -1,9 +1,10 @@
 package com.nitro_note.persistence.dao;
 
 import com.nitro_note.modelo.Mantenimiento;
+import com.nitro_note.persistence.dto.MantenimientoJPADTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MantenimientoDAO extends JpaRepository<Mantenimiento, Long>{}
+public interface MantenimientoDAO extends JpaRepository<MantenimientoJPADTO, Long>{}
 

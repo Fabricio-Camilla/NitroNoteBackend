bb99e3c361a3b2cb39d4ea011a41c1b31940eece
lucrecolon <lucrecolon1828@gmail.com>
2025-10-05 15:35:24 -0300

    arreglo de bug #64

--- src/main/java/ar/edu/unq/spring/controller/dto/UsuarioDTO.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unq.spring.controller.dto;
-
-import ar.edu.unq.spring.modelo.Usuario;
-
-public record UsuarioDTO(String email, String nombre, String role) {
-    public static UsuarioDTO desdeModelo(Usuario u) {
-        return new UsuarioDTO(u.getEmail(), u.getNombre(), "ROLE_USER");
-    }
-}

10ab920db4564d270dba94593e17c3745ba35dd4
Leandro <leandropeppe@gmail.com>
2025-10-08 16:20:41 -0300

    Se agrego query para buscar mantenimientos que vencen en el dia

--- src/main/java/ar/edu/unq/spring/persistence/MantenimientoDAO.java
+++ src/main/java/ar/edu/unq/spring/persistence/MantenimientoDAO.java
@@ -5,10 +5,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface MantenimientoDAO extends JpaRepository<MantenimientoJPADTO, Long> {
+
     @Query("SELECT m FROM Mantenimiento m WHERE m.vehiculo.usuarioID = :usuarioId")
     List<MantenimientoJPADTO> findByVehiculoUsuarioID(@Param("usuarioId") Long usuarioId);
+
+    @Query("""
+    SELECT m
+    FROM Mantenimiento m
+    JOIN FETCH m.vehiculo v
+    WHERE m.fechaARealizar = :hoy
+      AND m.finalizado = false
+""")
+    List<MantenimientoJPADTO> findMantenimientosQueVencenHoy(LocalDate hoy);
 }

00265f6b731057b5ff17fc00109983fe294ae45d
Fabricio-Camilla <fachucamilla32@gmail.com>
2025-10-27 14:39:41 -0300

    cambio horario final

--- src/main/java/ar/edu/unq/spring/service/scheduler/EmailScheduler.java
+++ src/main/java/ar/edu/unq/spring/service/scheduler/EmailScheduler.java
@@ -14,7 +14,7 @@ public class EmailScheduler {
     }
 
     // Ejecuta todos los días a las 9:00 AM hora Argentina
-    @Scheduled(cron = "00 00 08 * * *", zone = "America/Argentina/Buenos_Aires")
+    @Scheduled(cron = "00 50 21 * * *", zone = "America/Argentina/Buenos_Aires")
     public void enviarRecordatorios() {
         System.out.println("Ejecutando job diario de notificaciones...");
         notificationService.enviarRecordatoriosDelDia();
